feff9297350580fafcd12e52352f5fd5c2faa08a
Tiago Machado <timac1997@gmail.com>
2026-03-18 17:45:57 +0000

    init ligacao db

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
 const app = express()
 
 app.use(express.json())
 
+// middleware
 const logger = require("./middlewares/loggerMiddleware")
 app.use(logger)
 
@@ -10,30 +15,26 @@ app.use(logger)
 const taskRoutes = require("./routes/tasks")
 app.use("/tasks", taskRoutes)
 
-
 // users
 const userRoutes = require("./routes/users")
 app.use("/users", userRoutes)
-
 
 // tags
 const tagsRoutes = require("./routes/tags")
 app.use("/tags", tagsRoutes)
 
-
 // taskTags
 const taskTagsRoutes = require("./routes/taskTags")
 app.use("/taskTags", taskTagsRoutes)
 
-
 // notifications
 const notificationsRoutes = require("./routes/notifications")
 app.use("/notifications", notificationsRoutes)
-
 
 // taskUsers
 const taskUsersRoutes = require("./routes/taskUsers")
 app.use("/taskUsers", taskUsersRoutes)
 
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`))
