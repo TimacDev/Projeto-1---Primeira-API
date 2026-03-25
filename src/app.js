c91b3f0286d7d0e1c32d076c5924f804d155275f
Tiago Machado <timac1997@gmail.com>
2026-03-25 10:31:59 +0000

    cors update

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 
 // middleware
 const logger = require("./middlewares/loggerMiddleware");
